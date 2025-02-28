21ff8593c499ec10be76283ef6eafad9b4d3a699
Natan Paulo <natanpaulo1300@gmail.com>
2025-02-28 11:17:06 -0300

    Finalizado 2.0

---
+++ pagina questoes/quetions.js
@@ -0,0 +1,34 @@
+export default [
+    {
+      question: "Qual os principais fatores que contribuem com aquecimento global?",
+      answers: [
+        { option: "Emissão de gases, Queima de combustivéis, Desmatamento, Pecuaria, Poluição industrial", correct: true },
+        { option: "Rotação e tranlação da terra", correct: false },
+        { option: "Expansão de geladeiras", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o gás principal responsável pelo efeito estufa?",
+      answers: [
+        { option: "Oxigênio", correct: false },
+        { option: "Dióxido de carbono", correct: true },
+        { option: "Hélio", correct: false },
+      ],
+    },
+    {
+      question: "Qual dessas consequências está relacionada ao aquecimento global?",
+      answers: [
+        { option: "Redução da atividade vulcânica", correct: false },
+        { option: "Aumento de árvores", correct: false },
+        { option: "Aumento do nível do mar", correct: true },
+      ],
+    },
+    {
+      question: "O que pode ajudar a reduzir o aquecimento global?",
+      answers: [
+        { option: "Ampliar o desmatamento", correct: false },
+        { option: "Aumentar o uso de combustiveis fosseis", correct: false },
+        { option: "Aumentar uso de energias renóvaveis", correct: true },
+      ],
+    },
+  ];
